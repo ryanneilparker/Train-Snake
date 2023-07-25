bc03f7a36662ac905c4f0fcfb4470fcfa36d73c0
Gabriel da Silva <gabrield@bbd.co.za>
2023-07-25 14:35:09 +0200

    Server static files to api inside CSharp project

---
+++ TrainSnakeAPI/TrainSnakeAPI/StaticFiles/scripts/scores.js
@@ -0,0 +1,22 @@
+// Fetch data from the backend API
+fetch("https://api.example.com/scores")
+  .then((response) => response.json())
+  .then((data) => {
+    // Iterate over the scores and add rows to the scoreboard
+    const scoreboardBody = document.getElementById("table-body-scores");
+    data.forEach((score) => {
+      const row = document.createElement("tr");
+      const usernameCell = document.createElement("td");
+      const scoreCell = document.createElement("td");
+
+      usernameCell.textContent = score.username;
+      scoreCell.textContent = score.score;
+
+      row.appendChild(usernameCell);
+      row.appendChild(scoreCell);
+      scoreboardBody.appendChild(row);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching scores:", error);
+  });
