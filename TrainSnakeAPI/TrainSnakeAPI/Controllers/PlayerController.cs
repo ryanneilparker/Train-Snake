16912847e53dcefb294604cc68f522a0a65cf60c
Hsien Heng Lie <hsien@bbd.co.za>
2023-07-27 16:32:27 +0200

    reverted playerController

--- TrainSnakeAPI/TrainSnakeAPI/Controllers/PlayerController.cs
+++ TrainSnakeAPI/TrainSnakeAPI/Controllers/PlayerController.cs
@@ -51,8 +51,7 @@ namespace TrainSnakeAPI.Controllers
 		public IActionResult LoginUser()
 		{
 			Console.WriteLine("At login get");
-			var redirectUri = "http://et4whnrya5.eu-west-1.awsapprunner.com/githubOAuth";
-			return Challenge(new AuthenticationProperties { RedirectUri = redirectUri }, "github");
+      return Challenge(new AuthenticationProperties { RedirectUri = "/githubOAuth" }, "github");
 		}
 
 		[HttpGet("score")]
